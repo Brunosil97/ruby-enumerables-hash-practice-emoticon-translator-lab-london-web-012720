1e7e21fb2febf91a3861c46adff739178a4da016
Bruno Silva <brunosilvinosilva@gmail.com>
2020-01-13 15:32:21 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,6 +1,6 @@
 # require modules here
 require "yaml" 
-emoticons = YAML.load_file('.lib/emoticons.yml')
+emoticons = YAML.load_file('./lib/emoticons.yml')
 emoticons
 def load_library
   # code goes here
