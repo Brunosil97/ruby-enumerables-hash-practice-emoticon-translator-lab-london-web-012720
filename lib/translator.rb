48cd6d4b584b75dc3bb49db08b5da7fac6964502
Bruno Silva <brunosilvinosilva@gmail.com>
2020-01-13 15:40:42 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,7 +3,8 @@ require "yaml"
 emoticons = YAML.load_file('./lib/emoticons.yml')
 emoticons
 def load_library(emoticons)
- hash = {"get_meaning": {}}
+ hash = {
+   "get_meaning": {}}
   # code goes here
 end
 
