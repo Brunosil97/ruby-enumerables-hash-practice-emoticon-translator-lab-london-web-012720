5dd162f0f2b1779740cb5ca5df3df77dca469fa5
Bruno Silva <brunosilvinosilva@gmail.com>
2020-01-13 15:59:37 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -8,8 +8,8 @@ def load_library(file_path)
    'get_emoticon' => {}
  }
   emoticons.each do |key, value|
-   new_hash['get_emoticon'][value[0]] = emoticons[key][1] 
-   new_hash['get_meaning'][value[1]] = key 
+   new_hash['get_emoticon'][value[0]] = emoticons[key][1] #
+   new_hash['get_meaning'][value[1]] = key #saying what the japanese emoticons are in english 
  end 
  new_hash
 end
